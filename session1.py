bbceef23010072eead51443748cad3acfd082930
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-03-11 23:48:12 +0200

    add practicse of session one

---
+++ session1.py
@@ -0,0 +1,35 @@
+'''x=100
+if x>100:
+    print('x>100')
+elif x>200:
+    print('x>200')
+else:
+    print('done')'''
+
+username='admin'
+password=12345
+
+'''if username == 'admin' and password == 12345:
+    print('welcome')'''
+
+#true condition false
+    
+y=17    
+    
+#print('welcom') if y>19 else print('you are child')
+
+
+names = ['ahmed' , 'ali' ,'mahmoud']
+
+if 'ahmed' not in names:
+    print('welcom ahmed')
+#and == all
+#or == any    
+x=2;y=9;f=7
+if all([x==2,y==9,f==7]):
+    print('true')
+
+    
+
+
+    
