42187a9d1230fa3fae8e8aa9e266c9c878f74f96
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-03-12 16:58:52 +0200

    add session 3

---
+++ session2.py
@@ -0,0 +1,87 @@
+'''=0
+while x<10:
+    print(x)
+    x+=1
+#so infint loop is have a penifit for gui
+
+# while cold be in single line form
+while x<10:print(x);x+=1
+
+for letter in 'python':
+    print(letter)
+#range(start=0,end,step=1)'''
+
+    
+'''for x in [1,2,3,4,6,5,10]:
+    print(x)'''
+#for x in range(1,10):
+  #  print(x)
+'''for x in range(1,6):
+    for y in range(1,13):
+        print(f'{x}x{y}={x*y}')'''
+
+'''numbers=[[1,2,3,4],[5,6,7,8]]
+for y in numbers:
+    for x in y:
+        print(x)'''
+
+'''for x in range(30):
+    if x ==15:
+        break
+    print(x)'''
+'''for x in range(30):
+    if x ==15:
+        continue
+    print(x)'''
+'''print('Numbers\tSquare')
+print('------------------')
+for x in range(11):
+    print(f'{x}\t{x**2}')'''
+'''start=int(input('add your strat:'))
+end=int(input('add your end:'))
+for x in range(start):
+    for y in range(end):
+        print('*',end='')
+    print(' ')'''
+
+'''for x in range(1,9):
+    for c in range(x):
+        print('*',end='')
+    print(' ')
+
+for x in range(8):
+    print((x+1)*'*')'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#modular
+#argument parameters
+#requiered
+#keyword :positional
+#default
+def mysum(x,y):
+    return x+y
+
+l=mysum(3,4)
+print(l)
+
+        
+    
+    
